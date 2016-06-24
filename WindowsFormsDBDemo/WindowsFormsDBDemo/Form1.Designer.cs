8a0826924832b024cea05fdb286947083663ff6a
Adam Clements <aj.clements@live.co.uk>
2016-06-24 15:05:51 +0100

    database Demo

---
+++ WindowsFormsDBDemo/WindowsFormsDBDemo/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace WindowsFormsDBDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadPeople = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.textBoxPhoneNumber = new System.Windows.Forms.TextBox();
+            this.textBoxStreet = new System.Windows.Forms.TextBox();
+            this.textBoxHouseNumber = new System.Windows.Forms.TextBox();
+            this.textBoxSurname = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLoadPeople
+            // 
+            this.buttonLoadPeople.Location = new System.Drawing.Point(77, 324);
+            this.buttonLoadPeople.Name = "buttonLoadPeople";
+            this.buttonLoadPeople.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadPeople.TabIndex = 0;
+            this.buttonLoadPeople.Text = "Load People";
+            this.buttonLoadPeople.UseVisualStyleBackColor = true;
+            this.buttonLoadPeople.Click += new System.EventHandler(this.buttonLoadPeople_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(246, 324);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 1;
+            this.buttonNext.Text = "Next Entry";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.buttonNext_Click);
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.AutoSize = true;
+            this.buttonPrevious.Location = new System.Drawing.Point(418, 324);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(85, 23);
+            this.buttonPrevious.TabIndex = 2;
+            this.buttonPrevious.Text = "Previous Entry";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Click += new System.EventHandler(this.buttonPrevious_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(38, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "House Number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Street";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Phone";
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(128, 53);
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(283, 20);
+            this.textBoxFirstName.TabIndex = 8;
+            // 
+            // textBoxPhoneNumber
+            // 
+            this.textBoxPhoneNumber.Location = new System.Drawing.Point(128, 245);
+            this.textBoxPhoneNumber.Name = "textBoxPhoneNumber";
+            this.textBoxPhoneNumber.Size = new System.Drawing.Size(283, 20);
+            this.textBoxPhoneNumber.TabIndex = 9;
+            // 
+            // textBoxStreet
+            // 
+            this.textBoxStreet.Location = new System.Drawing.Point(128, 199);
+            this.textBoxStreet.Name = "textBoxStreet";
+            this.textBoxStreet.Size = new System.Drawing.Size(283, 20);
+            this.textBoxStreet.TabIndex = 10;
+            // 
+            // textBoxHouseNumber
+            // 
+            this.textBoxHouseNumber.Location = new System.Drawing.Point(128, 150);
+            this.textBoxHouseNumber.Name = "textBoxHouseNumber";
+            this.textBoxHouseNumber.Size = new System.Drawing.Size(283, 20);
+            this.textBoxHouseNumber.TabIndex = 11;
+            // 
+            // textBoxSurname
+            // 
+            this.textBoxSurname.Location = new System.Drawing.Point(128, 98);
+            this.textBoxSurname.Name = "textBoxSurname";
+            this.textBoxSurname.Size = new System.Drawing.Size(283, 20);
+            this.textBoxSurname.TabIndex = 12;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(271, 383);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 13;
+            this.buttonAdd.Text = "button1";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 435);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxSurname);
+            this.Controls.Add(this.textBoxHouseNumber);
+            this.Controls.Add(this.textBoxStreet);
+            this.Controls.Add(this.textBoxPhoneNumber);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonPrevious);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonLoadPeople);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadPeople;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.TextBox textBoxPhoneNumber;
+        private System.Windows.Forms.TextBox textBoxStreet;
+        private System.Windows.Forms.TextBox textBoxHouseNumber;
+        private System.Windows.Forms.TextBox textBoxSurname;
+        private System.Windows.Forms.Button buttonAdd;
+    }
+}
+
---
+++ WindowsFormsDBDemo/WindowsFormsDBDemo/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDBDemo
+{
+    public partial class Form1 : Form
+    {
+        List<Customer> listCustomers = new List<Customer>();
+        int customerListIndex = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoadPeople_Click(object sender, EventArgs e)
+        {
+            using (ClientsEntities1 myEntities = new ClientsEntities1())
+            {
+                var customers = from customer in myEntities.Customers
+                                select customer;
+                listCustomers = customers.ToList();
+            }
+            DisplayCustomer(customerListIndex);
+        }
+
+        private void DisplayCustomer(int customerId)
+        {
+            if (listCustomers[customerId] != null)
+            {
+                textBoxFirstName.Text = listCustomers[customerId].Forename;
+                textBoxSurname.Text = listCustomers[customerId].Surname;
+                textBoxHouseNumber.Text = (listCustomers[customerId].HouseNumber).ToString();
+                textBoxStreet.Text = listCustomers[customerId].Street;
+                textBoxPhoneNumber.Text = listCustomers[customerId].Phone;
+            }
+        }
+
+        private void buttonNext_Click(object sender, EventArgs e)
+        {
+            customerListIndex++;
+            if (customerListIndex >= listCustomers.Count)
+            {
+                customerListIndex = 0;
+            }
+            DisplayCustomer(customerListIndex);
+        }
+
+        private void buttonPrevious_Click(object sender, EventArgs e)
+        {
+            customerListIndex--;
+            if (customerListIndex < 0)
+            {
+                customerListIndex = listCustomers.Count - 1;
+            }
+            DisplayCustomer(customerListIndex);
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            //add record button
+            using (ClientsEntities1 myEntities = new ClientsEntities1())
+            {
+                Customer addedCustomer = new Customer();
+                // get customer data from screen
+                addedCustomer.Forename = textBoxFirstName.Text;
+                addedCustomer.Surname = textBoxSurname.Text;
+                addedCustomer.HouseNumber = Convert.ToInt32(textBoxHouseNumber.Text);
+                addedCustomer.Street = textBoxStreet.Text;
+                addedCustomer.Phone = textBoxPhoneNumber.Text;
+                //add new record to database - Customers is table name            
+                myEntities.Customers.Add(addedCustomer);
+                myEntities.SaveChanges();
+            }
+        }
+    }
+}
+
+
+
